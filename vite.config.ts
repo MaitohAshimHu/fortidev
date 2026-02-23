b2ad96b7ffddf9a59d0592cf7c50924b93fd7ad8
MaitohAshimHu <ashim.dev.sec@gmail.com>
2026-02-24 01:06:34 +0530

    fix: Replace Replit vite config with standard configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: "client",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "./src"),
+      "@": path.resolve(process.cwd(), "./client/src"),
     },
   },
 });
